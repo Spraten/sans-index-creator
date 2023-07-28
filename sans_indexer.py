74205a423ddb4c4fa65eb895d9bbc1efaf52bbfd
Spraten <92181911+Spraten@users.noreply.github.com>
2023-07-27 21:01:07 -0400

    Update sans_indexer.py
    Changes Made:
    
    Added New Input Argument: Introduced a new -w or --words-file argument to specify a file containing specific words/phrases/acronyms to be indexed.
    Output Filename Adjustment: Modified the default naming convention for the output file. The script now appends _index.txt to the input filename instead of replacing .txt with _index.txt.
    Optimized Word Stripping: Corrected and optimized the word stripping functionality where specific phrases were incorrectly truncated.
    Specific Words Indexing:
    Loaded specific words from the provided words file into a list.
    Initialized an index specifically for these words.
    Checked each page for these specific words and populated their respective indices.
    Merged Indices for Output: Combined the specific word index with the main index for the final output.
    Encoding Enhancements: Specified utf-8 encoding for file reads/writes to ensure proper handling of special characters.
    Code Organization: Improved the flow of the script and organized code sections for better clarity and readability.

--- sans_indexer.py
+++ sans_indexer.py
@@ -10,37 +10,46 @@ Usage:
 \t-i, --input-file: txt file of SANS book.
 \t-o, --output-file: file to save new index at.
 \t-n, --student-name: full name of student, used to split pages by delimiter.
+\t-w, --words-file: file containing specific words/phrases/acronyms to be indexed.
 """)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input-file", help="txt file of SANS book.")
 parser.add_argument("-o", "--output-file", help="output file of index.")
 parser.add_argument("-n", "--student-name", help="full name of student.")
+parser.add_argument("-w", "--words-file", help="file containing specific words/phrases/acronyms.")
 options = parser.parse_args(sys.argv[1:])
-
 
 if not options.input_file:
     exit(Usage.format("Please enter an index file.\n"))
 
 if not options.output_file:
-    options.output_file = options.input_file.replace(".pdf", "") + ".txt"
+    options.output_file = options.input_file.replace(".txt", "") + "_index.txt"
 
 delimeter = "Licensed To: "
 if options.student_name:
     delimeter += options.student_name
 
-# Get common english words
+# Load specific words
+specific_words = []
+if options.words_file:
+    with open(options.words_file, 'r', encoding='utf-8') as wf:
+        specific_words = [line.strip() for line in wf]
+
+# Get common English words
 common_words = rq.get("https://raw.githubusercontent.com/dwyl/english-words/master/words.txt").text.split("\n")
-# function to recursively strip given characters in a word
+
+# Function to recursively strip given characters in a word
 characters_to_strip = "()'\":,”“‘?;-•’—…[]!"
 phrases_to_strip = ["'s", "'re", "'ve", "'t", "[0]", "[1]", "[2]", "[3]", "[4]", "[5]", "[6]"]
+
 def strip_characters(word):
     word_length = len(word)
     word = word.replace("’", "'")
     while True:
         for phrase in phrases_to_strip:
             if word.endswith(phrase):
-                word = word[:len(phrase)]
+                word = word[:-len(phrase)]
         word = word.strip(characters_to_strip).rstrip(".")
         if len(word) == word_length:
             return word
@@ -49,73 +58,64 @@ def strip_characters(word):
 
 # Check that word should be added to index
 def word_is_eligible(word):
-    # Length check
     if len(word) < 3:
         return False
-    # Starts with number
     if word[0].isdigit():
         return False
-    # Not common english word
     if word.lower() in common_words or word.lower() + "s" in common_words:
         return False
-    # Not SANS url
     if word.startswith("http://") or word.startswith("https://"):
         return False
     return True
 
-# Get pages in pdf
-with open(options.input_file, "r") as f:
+# Get pages in the text file
+with open(options.input_file, "r", encoding="utf-8") as f:
     data = f.read()
     pages = data.split(delimeter)[1:]
+# Initialize specific word index
+specific_word_index = {word: [] for word in specific_words}
 
-
-# Get words per page
-index = {} # Stores page number and words on page
-total_words = [] # Stores all words said
+# Process each page to build the main index and specific word index
+index = {}
+total_words = []
 for page_idx, page in enumerate(pages):
-    # Recursively replace whitespace with one singular space
-    page = page.replace("\n", " ").replace("\t", " ")
-    page_len = len(page)
-    while True:
-        page = page.replace("  ", " ")
-        if len(page) == page_len:
-            break
-        else:
-            page_len = len(page)
-    # Trim whitespace
-    page = page.strip()
-    # Get words
+    page = page.replace("\n", " ").replace("\t", " ").replace("  ", " ").strip()
     words = page.split(" ")
     long_words = []
+
     for word in words:
-        # Strip some punctuation
-        word = strip_characters(word).lower()
-        # If threshhold met, append to index
-        if word_is_eligible(word):
-            total_words.append(word)
-            long_words.append(word)
+        word_stripped = strip_characters(word).lower()
+        if word_is_eligible(word_stripped):
+            total_words.append(word_stripped)
+            long_words.append(word_stripped)
     index[page_idx] = long_words
 
-# Get result strings
+    # Check for specific words in the word list and populate their index
+    for word in specific_words:
+        if word.lower() in page.lower():
+            specific_word_index[word].append(str(page_idx))
+
+# Combine the specific word index with the main index
 results = []
+for word, pages in specific_word_index.items():
+    if pages:  # If the word was found in any pages
+        results.append(f"{word}: {', '.join(sorted(set(pages)))}")
+
+# For the main index
 for word in set(total_words):
     pages_word_is_in = []
-    # Get page numbers
     for page in index.keys():
         if word in index[page]:
             pages_word_is_in.append(str(page))
 
     if len(pages_word_is_in) < 15:
-        joined_pagenums = ', '.join(pages_word_is_in)
-        # Only append if not page number
-        if word != joined_pagenums:
-            results.append(f"{word}: {', '.join(pages_word_is_in)}")
+        results.append(f"{word}: {', '.join(sorted(set(pages_word_is_in)))}")
 
-# Sort output
+# Sort and save the results
 results.sort(key=str.casefold)
 
-# Write output to file
-with open(options.output_file, "w") as f:
+with open(options.output_file, "w", encoding='utf-8') as f:
     for result in results:
         f.write(result + "\n")
+
 print(f"Written index to {options.output_file}")
